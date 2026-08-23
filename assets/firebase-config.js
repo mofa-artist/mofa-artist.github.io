6838f1aa2296bbaf5c5f9d3d727baa91bb67ac6d
DnB <dan.bouillet.14@gmail.com>
2026-08-23 20:56:50 +0200

    Delete assets directory

--- assets/firebase-config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyCAVTER3H4LCzYcAXxtS9eYGnxfAoM44og",
-  authDomain: "mofa-be132.firebaseapp.com",
-  projectId: "mofa-be132",
-  storageBucket: "mofa-be132.firebasestorage.app",
-  messagingSenderId: "510637232127",
-  appId: "1:510637232127:web:e5b0874f5ab6e1026fa1ca",
-  measurementId: "G-WH3P0MM1WG"
-};
